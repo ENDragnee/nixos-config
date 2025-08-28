060301b4f7b8a238802c4bf48a8647331bbca6ad
Mastwal Mesfin <126337103+ENDragnee@users.noreply.github.com>
2025-08-28 12:07:53 +0300

    update: moved the configs to configuration.nix

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, ... }: {
@@ -10,49 +12,7 @@
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
+        sops-nix.nixosModules.sops
       ];
     };
-  services.postgresql = {
-    enable = true;
-    initialScript = pkgs.writeText "init-db" ''
-      CREATE ROLE hms_user WITH LOGIN PASSWORD '...';
-      CREATE DATABASE hms_db WITH OWNER = hms_user;
-    '';
-    };
-  services.nginx = {
-    enable = true;
-    virtualHosts."hms.local" = {
-      forceSSL = false;
-      locations."/" = {
-        proxyPass = "http://localhost:3000";
-        };
-      };
-    };
-  systemd.services.hms-app = {
-    # This service will start after the network and database are ready
-    after = [ "network-online.target" "postgresql.service" ];
-    wants = [ "network-online.target" "postgresql.service" ];
-
-    # The command to start your app
-    # (Assumes you've built your Next.js app with Nix)
-    script = ''
-      # Set database credentials and other secrets here
-      export DATABASE_URL="postgres://hms_user:password@localhost/hms_db"
-      # Run the production server from the Nix build output of your app
-      exec ${pkgs.my-hms-app}/bin/start-server
-    '';
-
-    # Run the service as a dedicated, unprivileged user for security
-    serviceConfig = {
-      User = "hms";
-      Group = "hms";
-      Restart = "always"; # Automatically restart if it crashes
-      };
-    };
-  };
-  users.users.hms = {
-    isSystemUser = true;
-    group = "hms";
-  };
-  users.groups.hms = {};
 }
