e5e5643de2426546fc89fa3b22d23127ad4e557b
Mastwal Mesfin <126337103+ENDragnee@users.noreply.github.com>
2025-08-28 12:48:20 +0300

    update: added sop and flake to the config

--- flake.nix
+++ flake.nix
@@ -3,16 +3,36 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    sops-nix.url = "github:Mic92/sops-nix";
-    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, ... }: 
+  let
+    hms-app-pkg = import ./hms-app/default.nix {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    };
+  in 
+  {
+    packages.x86_64-linux.default = hms-app-pkg;
     nixosConfigurations.ascii = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = { 
+        inherit hms-app-pkg; 
+      };
       modules = [
         ./configuration.nix
         sops-nix.nixosModules.sops
+        home-manager.nixosModules.home-manager
       ];
     };
+  };
 }
